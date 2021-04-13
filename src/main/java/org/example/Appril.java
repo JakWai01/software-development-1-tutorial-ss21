09a41c48db6b4c0c1b8e29cfeeef6844c63702d2
JakWai01 <jakob.waibel@web.de>
2021-04-13 19:12:02 +0200

    feat: Add while loop

---
+++ src/main/java/org/example/Appril.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Appril {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int count = 0;
+        int zahl = 0;
+
+        while(count < 10) {
+            if (count % 2 != 0) {
+                System.out.println(zahl);
+            }
+
+            zahl++;
+            count++;
+        }
+
+        // System.out.println(count);
+    }
+}
