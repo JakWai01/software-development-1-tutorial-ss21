6ad99aa71f48808c18a9a7599c8486963e366e4f
JakWai01 <jakob.waibel@web.de>
2021-04-16 12:45:41 +0200

    feat: Add for loop

--- src/main/java/org/example/Appril.java
+++ src/main/java/org/example/Appril.java
@@ -5,9 +5,10 @@ public class Appril {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        int count = 0;
-        int zahl = 0;
+        //int count = 0;
+        //int zahl = 0;
 
+        /*
         while(count < 10) {
             if (count % 2 != 0) {
                 System.out.println(zahl);
@@ -16,7 +17,19 @@ public class Appril {
             zahl++;
             count++;
         }
-
+        */
         // System.out.println(count);
+        /*
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        */
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i + j + " ");
+            }
+            System.out.println(); // newline
+        }
+
     }
 }
