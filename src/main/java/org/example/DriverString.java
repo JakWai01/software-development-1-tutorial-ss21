baab7ceb6b00cbcc2e3b1a067317a88d4ea912eb
JakWai01 <jakob.waibel@web.de>
2021-05-12 20:22:19 +0200

    feat: Add changes from 2021-05-12

--- src/main/java/org/example/DriverString.java
+++ src/main/java/org/example/DriverString.java
@@ -6,5 +6,7 @@ public class DriverString {
         String john = new String("John");    // my.personal.String constructor accepting a java.lang.String argument
         System.out.println("Name: " + john); // john.toString() executes my.personal.String.toString()
         System.out.println(john.sayHello());
+
+        System.out.println(john);
     }
 }
